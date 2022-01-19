51160f26849ec33934f82b516678215953273f01
omergeek <omergeek2020@gmail.com>
2022-01-19 23:03:20 +0300

    initial commit

---
+++ vars/buildImage.groovy
@@ -0,0 +1,10 @@
+#!/usr/bin/env groovy
+
+def buildImage() {
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t omer2020/my-repo:2.0 .'
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'docker push omer2020/my-repo:2.0'
+    }
+}
